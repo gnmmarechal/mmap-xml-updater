fb4880fd975a5f503832b7ece2c2a4deb4421c55
gnmmarechal <gnmpolicemata91@gmail.com>
2016-04-17 21:16:13 +0100

    Initial commit nº2

---
+++ index.lua
@@ -0,0 +1,6 @@
+--This script downloads the latest master branch from ihaveamac/9.6-dbgen-xmls and extracts the files to SDMC:/mmap
+
+--Server variables
+xmlzipurl = "https://github.com/ihaveamac/9.6-dbgen-xmls/archive/master.zip"
+
+--
