0eb9beec32c87210a2dd740188da57c7f60a99d9
gnmmarechal <gnmpolicemata91@gmail.com>
2016-04-17 23:05:43 +0100

    Updated script

--- index.lua
+++ index.lua
@@ -63,11 +63,48 @@ function checkquit()
 	end
 end
 
+function nextscr(newscr)
+	scr = newscr
+	if newscr == -1 then
+		quit()
+	end
+end
+
+function waitscri(newscr,key)
+	if Controls.check(pad, key) and NOT Controls.check(oldpad, key) then
+		nextscr(newscr)
+	end
+end
 
 
 --Screens
+function head()
+		Screen.debugPrint(0,0,"MMAP XML Updater by gnmmarechal", white, TOP_SCREEN)
+		Screen.debugPrint(0,20,"===============================", red, TOP_SCREEN)
+end
+
 function mainscr() -- scr = 1
+	head()
+	Screen.debugPrint(0,60,"A: Update", white, TOP_SCREEN)
+	Screen.debugPrint(0,80,"B: Exit", white, TOP_SCREEN)
+	waitscri(2,KEY_A)
+	waitscri(-1,KEY_B)
+end
+
+function install()
+	head()
+	if wifion == 1 then
 	
+	else
+	 scr = 0
+	end
+end
+
+function wifierr() -- scr = 0
+	head()
+	Screen.debugPrint(0,60,"There is no internet connection.", red, TOP_SCREEN)
+	waitscri(-1,KEY_B)
+	waitscri(1,KEY_A)
 end
 
 oldpad = Controls.read()
@@ -77,7 +114,12 @@ oldpad = Controls.read()
 while true do
 	clear()
 	pad = Controls.read()
-	
+	if scr == 1 then
+		mainscr()
+	end
+	if scr == 2 then
+		install()
+	end
 
 	flip()
 end
